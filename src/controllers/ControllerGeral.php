f634909ce431101ec7a644fd55f4d2a2f04c79df
OtavioMC <mendoncacostaotavio@gmail.com>
2024-02-18 00:39:53 -0300

    STYLE: Corrige identação e namespace da Controller e da Service Gerais

--- src/controllers/ControllerGeral.php
+++ src/controllers/ControllerGeral.php
@@ -1,44 +1,44 @@
 <?php
 
-namespace customerapp\src\services;
+    namespace customerapp\src\controllers;
 
-use customerapp\src\interfaces\Controller;
-use customerapp\src\interfaces\Singleton;
+    use customerapp\src\interfaces\Controller;
+    use customerapp\src\interfaces\Singleton;
 
-abstract class ControllerGeral implements Controller, Singleton{
-    protected $service;
+    abstract class ControllerGeral implements Controller, Singleton{
+        protected $service;
 
-    protected function __construct() {
-        $nomeClasseService = str_replace('Controller', '' , get_class($this) ) . "Service";
-        $nomeClasseService = str_replace( "controllers", "services", $nomeClasseService );
-        $this->service = $nomeClasseService::getInstancia();
+        protected function __construct() {
+            $nomeClasseService = str_replace('Controller', '' , get_class($this) ) . "Service";
+            $nomeClasseService = str_replace( "controllers", "services", $nomeClasseService );
+            $this->service = $nomeClasseService::getInstancia();
+        }
+
+        abstract public static function getInstancia();
+
+        public function buscarPorId($id) {
+            return $this->service->buscarPorId($id);
+        }
+
+        public function buscarTodos(array $parametros = [], int $pagina = 1, int $itensPorPagina = null) {
+            return $this->service->buscarTodos( $parametros, $pagina, $itensPorPagina);
+        }
+
+        public function salvar($objeto) {
+            return $this->service->salvar($objeto);
+        }
+
+        public function excluir($id) {
+            return $this->service->excluir($id);
+        }
+
+        public function transformarEmObjetos(Array $corpo) {
+            return $this->service->transformarEmObjetos($corpo);
+        }
+
+        public function transformarEmObjeto(Array $corpo) {
+            return $this->service->transformarEmObjeto($corpo);
+        }
+
+        abstract public function criar( Array $corpo );
     }
-
-    abstract public static function getInstancia();
-
-    public function buscarPorId($id) {
-        return $this->service->buscarPorId($id);
-    }
-
-    public function buscarTodos(array $parametros = [], int $pagina = 1, int $itensPorPagina = null) {
-        return $this->service->buscarTodos( $parametros, $pagina, $itensPorPagina);
-    }
-
-    public function salvar($objeto) {
-        return $this->service->salvar($objeto);
-    }
-
-    public function excluir($id) {
-        return $this->service->excluir($id);
-    }
-
-    public function transformarEmObjetos(Array $corpo) {
-        return $this->service->transformarEmObjetos($corpo);
-    }
-
-    public function transformarEmObjeto(Array $corpo) {
-        return $this->service->transformarEmObjeto($corpo);
-    }
-
-    abstract public function criar( Array $corpo );
-}
--- src/services/ServiceGeral.php
+++ src/services/ServiceGeral.php
@@ -1,46 +1,46 @@
 <?php
 
-namespace customerapp\src\services;
+    namespace customerapp\src\services;
 
-use customerapp\src\interfaces\Service;
-use customerapp\src\interfaces\Singleton;
-use customerapp\src\DAOs\ClienteDAO;
+    use customerapp\src\interfaces\Service;
+    use customerapp\src\interfaces\Singleton;
+    use customerapp\src\DAOs\ClienteDAO;
 
-abstract class ServiceGeral implements Service, Singleton{
-    protected $dao;
+    abstract class ServiceGeral implements Service, Singleton{
+        protected $dao;
 
-    protected function __construct() {
-        $nomeClasseDAO = str_replace('Service', '' , $this::class ) . "DAO";
-        $nomeClasseDAO = str_replace("services", "DAOs", $nomeClasseDAO );
-        $this->dao = $nomeClasseDAO::getInstancia();
+        protected function __construct() {
+            $nomeClasseDAO = str_replace('Service', '' , $this::class ) . "DAO";
+            $nomeClasseDAO = str_replace("services", "DAOs", $nomeClasseDAO );
+            $this->dao = $nomeClasseDAO::getInstancia();
+        }
+
+        abstract public static function getInstancia();
+
+        public function buscarPorId($id) {
+            return $this->dao->buscarPorId($id);
+        }
+
+        public function buscarTodos(array $parametros = [], int $pagina = 1, int $itensPorPagina = null) {
+            return $this->dao->buscarTodos( $parametros, $pagina, $itensPorPagina);
+        }
+
+        public function salvar($objeto) {
+            $this->validar($objeto);
+            return $this->dao->salvar($objeto);
+        }
+
+        public function excluir($id) {
+            return $this->dao->excluir($id);
+        }
+
+        public function transformarEmObjetos(Array $corpo) {
+            return $this->dao->transformarEmObjetos($corpo);
+        }
+
+        public function transformarEmObjeto(Array $corpo) {
+            return $this->dao->transformarEmObjeto($corpo);
+        }
+
+        abstract public function validar( $objeto, array &$erros = []);
     }
-
-    abstract public static function getInstancia();
-
-    public function buscarPorId($id) {
-        return $this->dao->buscarPorId($id);
-    }
-
-    public function buscarTodos(array $parametros = [], int $pagina = 1, int $itensPorPagina = null) {
-        return $this->dao->buscarTodos( $parametros, $pagina, $itensPorPagina);
-    }
-
-    public function salvar($objeto) {
-        $this->validar($objeto);
-        return $this->dao->salvar($objeto);
-    }
-
-    public function excluir($id) {
-        return $this->dao->excluir($id);
-    }
-
-    public function transformarEmObjetos(Array $corpo) {
-        return $this->dao->transformarEmObjetos($corpo);
-    }
-
-    public function transformarEmObjeto(Array $corpo) {
-        return $this->dao->transformarEmObjeto($corpo);
-    }
-
-    abstract public function validar( $objeto, array &$erros = []);
-}
