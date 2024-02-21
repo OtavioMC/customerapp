f340217171244c3b7b1cf59ad8dea7d5bd537a93
OtavioMC <mendoncacostaotavio@gmail.com>
2024-02-21 06:35:39 -0300

    ADD: adiciona view de clientes e conclui a primeira iteração de /clientes

--- src/views/registrar-clientes.php
+++ src/views/registrar-clientes.php
@@ -1,85 +1,108 @@
 <?php
     require_once __DIR__ . "/../assets/header.html";
-
+    //TODO: melhorar validação dos campos pelo jquery.
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
 
 <body>
+    <br>
     <div class="container-xxl my-md-4 bd-layout">
-        <br>
-        <br>
-        <br>
-            <form id="registrarClientes" action="/clientes" method="post">
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <form id="registrarCliente" action="/clientes" method="post">
+            <div class="mb-3">
+                <label for="nome" class="form-label">Nome Completo</label>
+                <input type="text" class="form-control" id="nome" minlength="10" maxlength="280" required>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" required>
+            </div>
+            <div class="mb-3">
+                <div class="col-auto">
+                    <label for="senha" class="form-label">Senha</label>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Senha</label>
-                    <input type="senha" class="form-control" id="exampleInputPassword1">
+                <div class="col-auto">
+                    <input type="password" id="senha" class="form-control" maxlength="20" minlength="6"
+                        aria-describedby="passwordHelpInline" required>
                 </div>
-                <!-- <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">CPF</label>
-                    <input type="cpf" class="form-control" id="cpf">
-                </div> -->
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Concordo que por motivos do site se encontrar em desenvolvimento, vou inserir somente dados ficticios aqui por enquanto.</label>
+                <div class="col-auto">
+                    <span id="passwordHelpInline" class="form-text">
+                        Deve ter entre 6 a 20 caracteres.
+                    </span>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            </div>
+            <div class="mb-3">
+                <label for="cpf" class="form-label">CPF &nbsp;</label>
+                <input type="text" id="cpf" name="cpf" required />
+            </div>
+            <div class="mb-3">
+                <label for="data_nascimento">Data de Nascimento</label>
+                <input type="date" min="1900-01-01" max="2016-01-01" class="form-control" id="data_nascimento" name="data_nascimento" required>
+            </div>
+            <div class="mb-3 form-check">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                    <label class="form-check-label" for="invalidCheck">
+                        Concordo que por motivos do site se encontrar em desenvolvimento, vou inserir somente dados ficticios aqui por enquanto.
+                    </label>
+                    <div class="invalid-feedback">
+                        Você deve concordar antes de enviar.
+                    </div>
+                </div>
+            </div>
+            <button type="button" class="btn btn-primary" id="submitButton">Enviar</button>
+        </form>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous">
-
-        // jQuery.validator.addMethod("cpf", function(value, element) {
-        // value = jQuery.trim(value);
-
-        //     value = value.replace('.','');
-        //     value = value.replace('.','');
-        //     cpf = value.replace('-','');
-        //     while(cpf.length < 11) cpf = "0"+ cpf;
-        //     var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
-        //     var a = [];
-        //     var b = new Number;
-        //     var c = 11;
-        //     for (i=0; i<11; i++){
-        //         a[i] = cpf.charAt(i);
-        //         if (i < 9) b += (a[i] * --c);
-        //     }
-        //     if ((x = b % 11) < 2) { a[9] = 0 } else { a[9] = 11-x }
-        //     b = 0;
-        //     c = 11;
-        //     for (y=0; y<10; y++) b += (a[y] * c--);
-        //     if ((x = b % 11) < 2) { a[10] = 0; } else { a[10] = 11-x; }
-
-        //     var retorno = true;
-        //     if ((cpf.charAt(9) != a[9]) || (cpf.charAt(10) != a[10]) || cpf.match(expReg)) retorno = false;
-
-        //     return this.optional(element) || retorno;
-
-        // }, "Informe um CPF válido");
-
-
-        // $(document).ready(function(){
-
-        //     $("#registrarClientes").validate({
-        //         rules: {
-        //             cpf: {cpf: true, required: true}
-        //         },
-        //         messages: {
-        //             cpf: { cpf: 'CPF inválido'}
-        //         }
-        //         ,submitHandler:function(form) {
-        //             alert('ok');
-        //         }
-        //     });
-        // });
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js">
+    </script>
+    <script src="https://yandex.st/highlightjs/7.3/highlight.min.js"></script>
+    <script src="\vendor\twbs\bootstrap\dist\js\bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
 
-    <script src="\vendor\twbs\bootstrap\dist\js\bootstrap.bundle.min.js">
+    <script>
+    $(document).ready(function() {
+        $("#cpf").mask('000.000.000-00', {
+            reverse: true
+        });
+
+        $("#submitButton").click(function(e) {
+
+            var form = $("#registrarCliente")[0];
+
+
+            if (form.checkValidity() === false) {
+                form.classList.add('was-validated');
+                return;
+            }
+
+            var formData = {
+                nome: $("#nome").val(),
+                email: $("#email").val(),
+                senha: $("#senha").val(),
+                cpf: $("#cpf").val(),
+                data_nascimento: $("#data_nascimento").val()
+            };
+
+            $.ajax({
+                type: "POST",
+                url: "/clientes", // Sua rota POST /clientes
+                data: formData,
+                success: function(response) {
+                    console.log(response);
+                    alert("Cliente cadastrado com sucesso!");
+                    window.location.href = "/clientes/listar?alert=successCreate";
+                },
+                error: function(xhr, status, error) {
+                    alert("Erro ao cadastrar cliente. Detalhes: " + xhr.responseText);
+                }
+            });
+        });
+    });
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 </body>
